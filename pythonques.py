9374ea77806ed2b7024f5167391e0f81b184918d
NileshPatil1912 <nilesh.patil1912@gmail.com>
2023-05-06 16:18:52 +0530

    Added code questions

--- pythonques.py
+++ pythonques.py
@@ -6,3 +6,8 @@ dict1 = func('mom',48)
 dict2 = func('dad',56)
 
 print(dict1)
+
+def func(arr1, arr2):
+    return any(True if x==y else False for x in arr1 for y in arr2)
+
+print(func([1,2,3],[4,3,2,1]))
