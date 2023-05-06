f036ec7fd9846e7933de0620447e661da1522b62
NileshPatil1912 <nilesh.patil1912@gmail.com>
2023-05-06 16:18:45 +0530

    Python Study

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,2 +1,14 @@
 print ("Hello World");
 
+def my_dict():
+    d = {"name":"Bob","age":30}
+    return len(set(d))
+
+print (my_dict())
+
+
+languages =["Java","C++","Python","Javascript","Julia","Rust"]
+for lang in languages[:]:
+    languages.remove(lang)
+
+print(languages)
---
+++ pythonques.py
@@ -0,0 +1,8 @@
+def func(k,v,my_dict={}):
+    my_dict[k]=v
+    return my_dict
+
+dict1 = func('mom',48)
+dict2 = func('dad',56)
+
+print(dict1)
