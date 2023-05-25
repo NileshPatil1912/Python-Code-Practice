e6d8b59b310763646b538a10df24f4c30e1a6978
NileshPatil1912 <nilesh.patil1912@gmail.com>
2023-05-25 15:59:03 +0530

    Added Tricky Python Questions folder

---
+++ PythonQuestions/pythonquestion1.py
@@ -0,0 +1,13 @@
+age = 17
+
+if age >=18 or ...:
+    print("I am going to drive on my own")
+else:
+    print("I need a driver")
+
+
+print("Python"  "is"   "amazing")
+
+x = min((1,3,5),(2,),(1,2,8,9),(4,7))
+
+print(x)
